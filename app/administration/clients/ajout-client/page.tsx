d1bbd3066963e8cddefec08546a5e93e9732756e
TEST STAGIAIRE <TSTAGIAIRE@bionexx.loc>
2026-09-02 21:22:52 +0300

    add client

--- app/administration/clients/ajout-client/page.tsx
+++ app/administration/clients/ajout-client/page.tsx
@@ -1,10 +1,14 @@
-"use client"
+"use client";
 
 import TextHeading from "@/components/TextHeading";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 import { clientdescriptions } from "../page";
+import { useState } from "react";
+import { FaHandPointUp } from "react-icons/fa";
+import Image from "next/image";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 export const clientSchema = z.object({
   nom: z
@@ -46,7 +50,11 @@ export const clientSchema = z.object({
 });
 
 type ClientForm = z.infer<typeof clientSchema>;
+
 const AjoutClient = () => {
+  const [preview, setPreview] = useState<any>("");
+  const [file, setFile] = useState<any>("");
+
   const {
     register,
     handleSubmit,
@@ -69,6 +77,24 @@ const AjoutClient = () => {
     },
   });
 
+  const handleFileChange = (e: any) => {
+    const selectedImage = e.target.files[0];
+    if (selectedImage) {
+      setFile(selectedImage);
+      setPreview(URL.createObjectURL(selectedImage));
+    }
+  };
+
+  const [focus, setFocus] = useState({
+    nom: false,
+    prenoms: false,
+    sexe: false,
+    email: false,
+    tel: false,
+    nationalite: false,
+    adresse: false,
+  });
+
   const submitData = async (data: ClientForm) => {
     await new Promise((resolve) => setTimeout(resolve, 3000));
     console.log("Données du formulaire :", data);
@@ -89,8 +115,158 @@ const AjoutClient = () => {
           Veuillez remplir les informations
         </p>
 
-        <div className="w-full h-[200px] border border-dashed border-border rounded-lg mt-4">
+        <div className="w-full h-[150px] border border-dashed border-border rounded-lg mt-4 flex justify-center relative">
+          <input
+            type="file"
+            accept="image/*"
+            className="absolute inset-0 opacity-0 cursor-pointer z-20"
+            onChange={(e) => handleFileChange(e)}
+          />
+          {file === "" ? (
+            <p className="h-full flex items-center text-xs italic text-muted-foreground cursor-pointer">
+              <FaHandPointUp size={14} className="mr-2" />
+              Cliquer ici pour charger l'image
+            </p>
+          ) : (
+            <div className="w-[280px] h-full bg-muted relative">
+              {preview && (
+                <Image
+                  src={preview}
+                  alt="client"
+                  fill
+                  className="object-cover z-10"
+                />
+              )}
+            </div>
+          )}
+        </div>
 
+        <div className="flex flex-col sm:flex-row gap-[10px] w-full mt-8 items-start">
+          <div className="flex-1 relative w-full">
+            <label
+              className={`mb-1 absolute text-[12px] ml-2 bg-card  px-3
+                  ${focus.nom ? "top-[-10px] text-foreground font-semibold" : "top-[11px] text-muted-foreground capitalize"} transition-all duration-400`}
+            >
+              nom
+            </label>
+
+            <input
+              type="text"
+              {...register("nom")}
+              onFocus={() => setFocus({ ...focus, nom: true })}
+              onBlur={() => {
+                const nom = getValues("nom");
+
+                if (nom.trim() === "") {
+                  setFocus({ ...focus, nom: false });
+                } else {
+                  setFocus({ ...focus, nom: true });
+                }
+              }}
+              className="w-full rounded border px-4 py-3 outline-none text-xs"
+            />
+
+            {errors.nom && (
+              <p className=" text-[10px] text-red-500">{errors.nom.message}</p>
+            )}
+          </div>
+          <div className="flex-1 relative w-full">
+            <label
+              className={`mb-1 absolute text-[12px] ml-2 bg-card  px-3
+                  ${focus.prenoms ? "top-[-10px] text-foreground font-semibold" : "top-[11px] text-muted-foreground capitalize"} transition-all duration-400`}
+            >
+              Prenoms
+            </label>
+
+            <input
+              type="text"
+              {...register("prenoms")}
+              onFocus={() => setFocus({ ...focus, prenoms: true })}
+              onBlur={() => {
+                const prenoms = getValues("prenoms");
+
+                if (prenoms.trim() === "") {
+                  setFocus({ ...focus, prenoms: false });
+                } else {
+                  setFocus({ ...focus, prenoms: true });
+                }
+              }}
+              className="w-full rounded border px-4 py-3 outline-none text-xs"
+            />
+
+            {errors.prenoms && (
+              <p className=" text-[10px] text-red-500">
+                {errors.prenoms.message}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className="flex flex-col sm:flex-row gap-[10px] w-full mt-8 items-start">
+           <div className="flex-1 relative w-full">
+              <Controller
+                name="sexe"
+                control={control}
+                render={({ field }) => (
+                  <Select value={field.value} onValueChange={field.onChange}>
+                    <SelectTrigger className="w-full rounded! border h-auto! px-4 py-3 outline-none text-xs">
+                      <SelectValue placeholder="Sexe" />
+                    </SelectTrigger>
+
+                    <SelectContent >
+                      <SelectItem
+                        value="Homme"
+                        className="py-3 px-3 text-xs"
+                        
+                      >
+                        Homme
+                      </SelectItem>
+
+                      <SelectItem
+                        value="Femme"
+                        className="py-3 px-3 text-xs"
+                      >
+                        Femme
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+              {errors.sexe && (
+                <p className=" text-[10px] text-red-500">
+                  {errors.sexe.message}
+                </p>
+              )}
+            </div>
+          <div className="flex-1 relative w-full">
+            <label
+              className={`mb-1 absolute text-[12px] ml-2 bg-card  px-3
+                  ${focus.email ? "top-[-10px] text-foreground font-semibold" : "top-[11px] text-muted-foreground capitalize"} transition-all duration-400`}
+            >
+              Email
+            </label>
+
+            <input
+              type="text"
+              {...register("email")}
+              onFocus={() => setFocus({ ...focus, email: true })}
+              onBlur={() => {
+                const email = getValues("email");
+                if (email.trim() === "") {
+                  setFocus({ ...focus, email: false });
+                } else {
+                  setFocus({ ...focus, email: true });
+                }
+              }}
+              className="w-full rounded border px-4 py-3 outline-none text-xs"
+            />
+
+            {errors.email && (
+              <p className=" text-[10px] text-red-500">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
         </div>
       </form>
     </div>
